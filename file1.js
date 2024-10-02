c1055b78052baaf10b9dfc8d44b9e66a755a4ae3
Ethan Zhao <ethanzhao1981@gmail.com>
2024-10-01 22:21:00 -0400

    the storm before the calm
    good luck with the interviews!

---
+++ file1.js
@@ -0,0 +1,15 @@
+/**
+ * Welcome, to the holy land of file1.js!
+ * Below are some features that I've been too lazy to implement
+ * just uncomment them to implement them, if you don't mind.
+ */
+
+// function feature1() {
+//  console.log("FEATURE 1 IS SO COOL");
+//  return new Promise(() => "to be cool");
+// }
+
+// function feature2() {
+//   console.log("FEATURE 1 IS KINDA A LIAR");
+//   return new Promise((resolve, reject) => reject("to be cool"));
+// }
---
+++ file2.js
@@ -0,0 +1,5 @@
+console.log("what is my purpose?");
+
+setTimeout(() => {
+  console.log("You pass butter.");
+}, 2000);
